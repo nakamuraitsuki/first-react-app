9f907cfebb04a13852c24e9d06b4b2b8a82f0e35
nakamura <itsuki1911n@gmail.com>
2023-06-19 19:28:05 +0900

    todoを追加する処理を実装

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,25 @@ import { useState } from "react";
 function App() {
   const [todoInput, setTodoInput] = useState("");
   const [todos, setTodos] = useState(["掃除", "洗濯", "買い物"]);
+
   function handleChange(event) {
     setTodoInput(event.target.value);
+  };
+
+  function handleClick() {
+    const newTodos = [...todos, todoInput];
+    setTodos(newTodos);
+    setTodoInput("");
   };
 
   return (
     <div>
       <input type="text" value={todoInput} onChange={handleChange} />
-      {todos.map((todo, i) => <p key={i}>{todo}</p>)}
+      <button onClick={handleClick}>追加</button>
+      {todos.map((todo,i) => <p key={i}>{todo}</p>)}
     </div>
   );
 };
+
 
 export default App;
